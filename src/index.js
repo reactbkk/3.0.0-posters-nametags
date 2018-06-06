d514153071b7d71c95ef35930c55db65e1f5cbb3
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2018-06-07 00:29:20 +0700

    - change to hash router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import './design'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 function Text({ children, tight }) {
   return (
