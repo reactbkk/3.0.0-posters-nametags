1942b60987c10e5e3fb180a5f39ef7742696a2f2
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-06-15 23:41:50 +0700

    :sparkles: Fixd

--- src/attendee-tag/NameCardPage.js
+++ src/attendee-tag/NameCardPage.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { FIRST_BATCH_PRINTED } from './printed'
+import { overrides } from './overrides'
 
 const data = require('../data/attendees.json')
+
+console.log(data)
 
 /** @type {React.SFC<any>} */
 export function NameCardPage({ index }) {
@@ -8,83 +12,102 @@ export function NameCardPage({ index }) {
   return (
     <div>
       {c.map((t, i) => {
-        return (
-          <div
-            style={{
-              display: 'inline-block',
-              border: '0.1cm solid #ccc',
-              margin: '0.5cm'
-            }}
-          >
+        return <NameCard entry={t} key={t['Ticket']} />
+      })}
+    </div>
+  )
+}
+
+/** @type {React.SFC<any>} */
+function NameCard({ entry: t }) {
+  const background = /General/.test(t['Ticket type'])
+    ? `url(${require('./namecard-general.png')})`
+    : '#000'
+
+  const override = overrides[t['Ticket']] || {}
+
+  let position = t['ตำแหน่ง / ชั้นปี']
+  if (override.year) {
+    position = `นักศึกษาปี ${position}`
+  }
+
+  const sth = s =>
+    String(s || '')
+      .replace(/^[ืิ]*/, '')
+      .replace(/^\s*-\s*$/, '')
+
+  return (
+    <div
+      style={{
+        display: 'inline-block',
+        border: '0.1cm solid #ccc',
+        margin: '0.5cm',
+        opacity: FIRST_BATCH_PRINTED.has(t['Ticket']) ? 0.25 : 1
+      }}
+    >
+      <div
+        className="namecard"
+        onClick={() => prompt(`${t['Reference code']}`, t['Ticket'])}
+        data-id={t['Order number'].replace(/[#\s]/g, '')}
+        style={{
+          width: '1181px',
+          height: '1771px',
+          background: `${background} center no-repeat`,
+          backgroundSize: 'cover',
+          overflow: 'hidden'
+        }}
+      >
+        <div
+          style={{
+            fontFamily: 'Metropolis, Noto Sans Thai UI',
+            padding: '64px',
+            paddingTop: '1050px',
+            height: '480px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            wordWrap: 'break-word',
+            color: '#fff'
+          }}
+        >
+          <div>
             <div
-              className="namecard"
-              onClick={() => prompt('ID', t['Order number'])}
-              key={i}
-              data-id={t['Order number'].replace(/[#\s]/g, '')}
               style={{
-                width: '1181px',
-                height: '1771px',
-                background: `url(${require('./namecard.png')}) center no-repeat`,
-                backgroundSize: 'cover',
-                overflow: 'hidden'
+                fontSize: `${120 * (override.nameScale || 1)}px`,
+                letterSpacing: `${(override.tight || 0) * -0.04}em`,
+                lineHeight: '1.2em',
+                fontWeight: 700
               }}
             >
+              {sth(override.name || t['ชื่อที่แสดงบนป้ายชื่อ'])
+                .split('\n')
+                .map((x, i) => (
+                  <Fragment key={i}>
+                    {i > 0 && <br />}
+                    {x}
+                  </Fragment>
+                ))}
+            </div>
+            <div
+              style={{
+                fontSize: '48px',
+                lineHeight: '1.3em',
+                fontWeight: 700
+              }}
+            >
+              <div>{sth(override.position || position)}</div>
               <div
                 style={{
-                  fontFamily: 'Metropolis, Noto Sans Thai UI',
-                  padding: '64px',
-                  paddingTop: '1050px',
-                  height: '480px',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  textAlign: 'center',
-                  wordWrap: 'break-word',
-                  color: '#fff'
+                  color: '#858585'
                 }}
               >
-                <div>
-                  <div
-                    style={{
-                      fontSize: '120px',
-                      lineHeight: '1.2em',
-                      fontWeight: 700
-                    }}
-                  >
-                    {t['ชื่อที่แสดงบนป้ายชื่อ']}
-                  </div>
-                  {/* <div
-                  style={{
-                    fontSize: '0.64cm',
-                    lineHeight: '0.8cm',
-                    fontWeight: '600'
-                  }}
-                >
-                  {t['Firstname']} {t['Lastname']}
-                </div> */}
-                  <div
-                    style={{
-                      fontSize: '48px',
-                      lineHeight: '1.3em',
-                      fontWeight: 700
-                    }}
-                  >
-                    {t['ตำแหน่ง / ชั้นปี']}
-                    <br />
-                    <div
-                      style={{
-                        color: '#858585'
-                      }}
-                    >
-                      {t['บริษัท / สถาบันการศึกษา']}
-                    </div>
-                  </div>
-                </div>
+                {sth(override.company || t['บริษัท / สถาบันการศึกษา'])}
               </div>
             </div>
           </div>
-        )
-      })}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/attendee-tag/overrides.js
@@ -0,0 +1,67 @@
+export const overrides = {
+  '1816859': {
+    year: true
+  },
+  '1816864': {
+    tight: true
+  },
+  '1816875': {
+    nameScale: 0.8,
+    company: 'บริษัท เซ็นด์อิท (ประเทศไทย) จำกัด'
+  },
+  '1816891': {
+    year: true
+  },
+  '1816902': {
+    year: true
+  },
+  '1816917': {
+    year: true
+  },
+  '1816927': {
+    position: 'Frontend developer'
+  },
+  '1816930': {
+    company: 'บริษัท เซ็นด์อิท (ประเทศไทย) จำกัด'
+  },
+  '1816938': { year: true },
+  '1816942': { year: true },
+  '1816952': { year: true },
+  '1816954': { year: true },
+  '1816974': { year: true },
+  '1817336': {
+    position: 'Developer',
+    nameScale: 0.8
+  },
+  '1822013': { year: true },
+  '1829980': { year: true },
+  '1829995': { year: true },
+  '1830011': { year: true },
+  '1830012': { year: true },
+  '1830040': { year: true },
+  '1830044': { year: true },
+  '1830046': { year: true },
+  '1830048': { year: true },
+  '1830085': { year: true },
+  '1830091': { year: true },
+  '1830094': { year: true },
+  '1830104': { year: true },
+  '1830106': { year: true },
+  '1830109': { year: true },
+  '1830124': { year: true },
+  '1830127': { year: true },
+  '1830146': { year: true },
+  '1830452': { nameScale: 0.8 },
+  '1830462': { nameScale: 0.8 },
+  '1837976': { nameScale: 0.8 },
+  '1839126': { company: 'Pronto Tools' },
+  '1839127': { year: true },
+  '1839192': { name: 'Shane\n@ Gummy Bear' },
+  '1839202': { name: 'Kevin\n@ Gummy Bear' },
+  '1840728': { year: true },
+  '1841426': { year: true },
+  '1841501': { position: 'frontend' },
+  '1841502': { position: 'frontend' },
+  '1841503': { position: 'frontend' },
+  '1841536': { name: 'MAN\nChanamueng' }
+}
